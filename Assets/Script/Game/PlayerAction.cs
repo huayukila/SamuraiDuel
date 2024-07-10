1ad0e9c7c9fb90b3d9538dfb41ba8762b17eb98a
huayukila <66170588+huayukila@users.noreply.github.com>
2024-07-10 12:00:39 +0900

    Update PlayerAction.cs

--- Assets/Script/Game/PlayerAction.cs
+++ Assets/Script/Game/PlayerAction.cs
@@ -1,5 +1,14 @@
 public abstract class PlayerAction
 {
+<<<<<<< Updated upstream
+=======
+PlayerController playerCtrl;
+    public PlayerAction(PlayerController ctlr){
+	playerCtrl=ctlr;
+	Init();
+}
+	protected abstract void Init();
+>>>>>>> Stashed changes
     public abstract void Update();
     public abstract void FixedUpdate();
 }
